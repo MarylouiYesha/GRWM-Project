e0b09fe7867142b31c89f344c19c959fbac8cc8d
MarylouiYesha <yesha.maryloui@gmail.com>
2023-02-15 00:35:34 +0800

    play button

--- musicplayer.py
+++ musicplayer.py
@@ -1,5 +1,6 @@
 from tkinter import*
 import tkinter as tk
+import fnmatch
 from tkinter import ttk, filedialog
 from pygame import mixer 
 import os
@@ -11,6 +12,10 @@ canvas.configure(bg="#0f1a2b")
 canvas.resizable(False,False)
 
 mixer.init()
+
+#button
+play_button=PhotoImage(file="play.jpg")
+Button(canvas, image=play_button, bg="#0f1")
 
 
 
