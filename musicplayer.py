4f08c230f5be436a84fbb9af000fc60fc36e744f
MarylouiYesha <yesha.maryloui@gmail.com>
2023-02-14 20:46:49 +0800

    mainloop

--- musicplayer.py
+++ musicplayer.py
@@ -2,3 +2,12 @@ import tkinter as tk
 import fnmatch
 import os
 from pygame import mixer
+
+canvas= tk.Tk()
+canvas.title("Music Player")
+canvas.geometry("600x800")
+canvas.config(bg='black')
+
+rootpath = ""
+
+canvas.mainloop()
