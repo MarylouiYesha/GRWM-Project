d86da44f62368ec73837e345f586070f7ba95a02
MarylouiYesha <yesha.maryloui@gmail.com>
2023-02-15 09:21:22 +0800

    play function

--- musicplayer.py
+++ musicplayer.py
@@ -5,17 +5,27 @@ from tkinter import ttk, filedialog
 from pygame import mixer 
 import os
 
+mixer.init()
+
+def play_song(song_name: StringVar, songs_list: Listbox, status: StringVar):
+    song_name.set(songs_list.get(ACTIVE))
+
+    mixer.music.load(songs_list.get(ACTIVE))
+    mixer.music.play()
+
+    status.set("Song PLAYING")
+
 canvas= tk.Tk()
 canvas.title("Music Player")
 canvas.geometry("920x670+290+85")
 canvas.configure(bg="#0f1a2b")
 canvas.resizable(False,False)
 
-mixer.init()
+
 
 #button
 play_button=PhotoImage(file="play.jpg")
-Button(canvas, image=play_button, bg="#0f1")
+Button(canvas, image=play_button, bg="#0f1").place(x=100,y=400)
 
 
 
