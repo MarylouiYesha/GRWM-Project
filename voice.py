e95da5ed397fbe88977d59946996b0f44d77cd57
MarylouiYesha <yesha.maryloui@gmail.com>
2023-02-18 20:15:04 +0800

    def talk

--- voice.py
+++ voice.py
@@ -2,3 +2,10 @@ import speech_recognition as sr
 import pyttsx3
 import pywhatkit
 
+def talk(command):
+    engine=pyttsx3.init()
+    voice=engine.getProperty('voices')
+    engine.setProperty('voice',voice[1].id)
+    engine.say("Playing "+ command)
+    engine.runAndWait()
+
