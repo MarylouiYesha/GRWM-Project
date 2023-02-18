1c9428079eb7a27d8d4bf29b1e8f9f5898318004
MarylouiYesha <yesha.maryloui@gmail.com>
2023-02-19 03:33:24 +0800

    final

--- musicplayer.py
+++ musicplayer.py
@@ -64,7 +64,7 @@ root.geometry('950x500')
 root.title('Music Player')
 root.resizable(0, 0)
 
-labelbg=PhotoImage(file="labelbg.png")
+labelbg=PhotoImage(file="labelbg (3).png")
 pause_img = tk.PhotoImage(file ='pause.png')
 play_img = tk.PhotoImage(file="play.png")
 resume_img= tk.PhotoImage(file="resume.png")
@@ -73,21 +73,21 @@ search_img= tk.PhotoImage(file="search.png")
 control=PhotoImage(file= "gray.png")
 
 
-song_frame = Label(root, image=labelbg, width=700, height=250)
+song_frame = Label(root, image=labelbg, width=700, height=450)
 song_frame.place(x=0, y=0)
 
 button_frame = Label(root, image=control, width=950, height=100)
 button_frame.place(y=380)
 
 
-listbox_frame = LabelFrame(root, text='Playlist', bg='RoyalBlue')
-listbox_frame.place(x=650, y=0, height=500, width=300)
+listbox_frame = LabelFrame(root, text='Playlist', font='Courier')
+listbox_frame.place(x=650, y=0, height=1000, width=300)
 
 current_song = StringVar(root, value='<Not selected>')
 
 song_status = StringVar(root, value='<Not Available>')
 
-playlist = Listbox(listbox_frame, font=('Helvetica', 11), selectbackground='Gold')
+playlist = Listbox(listbox_frame, font=('Courier', 11), selectbackground='Gold', width=1000, height=300)
 
 scroll_bar = Scrollbar(listbox_frame, orient=VERTICAL)
 scroll_bar.pack(side=RIGHT, fill=BOTH)
@@ -101,33 +101,37 @@ playlist.pack(fill=BOTH, padx=5, pady=5)
 Label(song_frame, text='C U R R E N T L Y    P L A Y I N G:', font=('Courier', 10, 'bold'), height=1, width=65).place(x=-1, y=-2)
 
 song_lbl = Label(song_frame, textvariable=current_song, bg='White', font=("Courier", 9), width=30, height=1)
-song_lbl.place(x=180, y=200)
+song_lbl.place(x=174, y=350)
 
 pause_btn = Button(button_frame, image= pause_img, height=30, width=30,
                     command=lambda: pause_song(song_status))
-pause_btn.place(x=15, y=10)
+pause_btn.place(x=205, y=10)
 
 stop_btn = Button(button_frame, image= stop_img, height= 30, width=30,
                   command=lambda: stop_song(song_status))
-stop_btn.place(x=105, y=10)
+stop_btn.place(x=247, y=10)
 
 play_btn = Button(button_frame, image= play_img, height=30,width=30,
                   command=lambda: play_song(current_song, playlist, song_status))
-play_btn.place(x=195, y=10)
+play_btn.place(x=290, y=10)
 
 resume_btn = Button(button_frame, image= resume_img, height=30, width=30,
                     command=lambda: resume_song(song_status))
-resume_btn.place(x=285, y=10)
-
-load_btn = Button(button_frame, text='Load Directory', font=("Courier", 9), width=35,
-                  command=lambda: load(playlist))
-load_btn.place(x=70, y=55)
+resume_btn.place(x=333, y=10)
 
 search_btn = Button(button_frame, image= search_img, height=30, width=30,
                   command=lambda: takeCommand())
-search_btn.place(x=300, y=10)
+search_btn.place(x=375, y=10)
 
-Label(root, textvariable=song_status, bg='SteelBlue', font=('Times', 9), justify=LEFT).pack(side=BOTTOM, fill=X)
+
+
+
+
+load_btn = Button(button_frame, text='Load Directory', font=("Courier", 9), width=35,
+                  command=lambda: load(playlist))
+load_btn.place(x=145, y=55)
+
+Label(root, textvariable=song_status, bg='White', font=('Times', 9), justify=LEFT).pack(side=BOTTOM, fill=X)
 
 root.update()
 root.mainloop()
